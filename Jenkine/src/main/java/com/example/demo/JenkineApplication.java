e5478fb08e979b74f44c812f893244868b326cfc
kavita <kavitachaudhari9689@gmail.com>
2022-08-02 20:50:50 +0530

    prject commit

---
+++ Jenkine/src/main/java/com/example/demo/JenkineApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkineApplication.class, args);
+	}
+
+}
